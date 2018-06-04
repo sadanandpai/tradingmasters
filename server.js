37122807415a5a7dac5a7e592ab6d789fdf23b96
Sadanand Pai <SESA497240@schneider-electric.com>
2018-06-04 22:22:07 +0530

    Server

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
 const express = require('express');
 var app = express();
 const bodyParser = require('body-parser');
+app.use(express.static('public'));
 const PORT = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 
+app.get("/", function(req, res){
+    res.sendFile( __dirname + "/" + "index.html");
+});
+
+
 var data = [];
-app.get('/',function(req, res){
+app.get('/rest/',function(req, res){
     res.status(200).send(data);
 })
 
-app.post('/',function(req, res){
+
+app.post('/rest/',function(req, res){
     data.push(req.body);
-    res.status(200).send(data);
+    res.status(200).send("Ok");
 })
+
+
+app.delete('/rest/:index',function(req, res){
+    data.splice(req.params.index, 1);
+    res.status(200).send("Ok");
+})
+
 
 app.listen(PORT, ()=>{console.log("this server is up on port " + PORT);})
