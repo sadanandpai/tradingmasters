a18f4069e3f3d1b77b4acae2be4b752e132eb3fc
Sadanand Pai M <SESA497240@schneider-electric.com>
2018-11-29 22:12:06 +0530

    Pagination

--- public/stock-list/stock-list.component.js
+++ public/stock-list/stock-list.component.js
@@ -1,7 +1,7 @@
 angular.module('stockList')
     .component('stockList', {
         templateUrl: 'stock-list/stock-list.template.html',
-        controller: ['$scope', '$http', '$window', 'stockData', function StockListController($scope, $http, $window, stockData){
+        controller: ['$scope', '$http', '$window', '$filter', 'stockData', function StockListController($scope, $http, $window, $filter, stockData){
             var self = this;
             self.load = true;
             self.stocks = [];
@@ -9,11 +9,13 @@ angular.module('stockList')
             self.reverse = true;
             self.errorMsg = 'No data found';
             self.opacity = "loader";
+            self.currentPage = "1";
 
             this.deleteStock = function deleteStock(index){
                 $http.delete("https://tradingmasters.herokuapp.com/rest/"+index).then(
                     function(response){
                         self.stocks.splice(index, 1);
+                        self.setPage();
                     }
                 )
             }
@@ -30,6 +32,7 @@ angular.module('stockList')
                 }
             ).then(function loadCurrentPrice(){
                     var promises = [];
+                    self.stockLength = self.stocks.length;
                     self.stocks.forEach(function(stock, index) {          
                         promises.push($http.get("https://www.quandl.com/api/v3/datasets/NSE/" + stock.stockName + ".json?api_key=gwZhGszfyS5bH7p44UfA&rows=1").then(
                             function(response){
@@ -53,7 +56,18 @@ angular.module('stockList')
             ).catch(function(){
                 self.errorMsg = "Error while retriving data";
             });
+
+            self.setPage = function(){
+                self.stockLength = $filter('filter')(self.stocks, self.search).length;
+            }
                       
         }]
     }
-)
+);
+
+
+angular.module('stockList').filter('startFrom', function() {
+    return function(input, start) {
+        return input.slice(start);
+    }
+});
